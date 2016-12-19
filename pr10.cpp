1c7f72262dbbe5bbb4323957e9fe831f12b3976b
Zmenkov Dima <dimamatem@gmail.com>
2016-12-19 19:40:00 +0200

    remade pr10.cpp

--- pr10.cpp
+++ pr10.cpp
@@ -16,16 +16,28 @@ bool test1(char ch)
 {
 	return(ch >= 'a')&&(ch <= 'z')||(ch >= 'A')&&(ch <= 'Z');
 }
+
+int toupper ( int c )
+
+{return c+'A' - 'a';
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {int maxSize = 30;
 char *str = new char[maxSize];
+
 cout<<"Input  string"<<endl;
 cin.getline(str, maxSize);
+	
 	int length = strlen(str);
-	if(test(str[0])) str[0] -= 32;
+	if(test(str[0])) str[0] =  toupper(str[0]) ;
 	for(int i=0;i<length-1;i++)
-	if((!test1(str[i]))&&(test(str[i+1]))) str[i+1] -= 32;
+	if((!test1(str[i]))&&(test(str[i+1]))) str[i+1] = toupper(str[i+1]) ;
 		cout<<endl;
-cout<<"new string"<<endl;
-cout<<str<<endl;
+
+		cout<<"new string"<<endl;
+       cout<<str<<endl;
+
+delete[] str;
+str=0;
 }
