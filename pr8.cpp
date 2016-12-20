70153f73e30a6d338b0730b5c5e61321de2d083c
Zmenkov Dima <dimamatem@gmail.com>
2016-12-20 15:05:47 +0200

    remade pr8.cpp

--- pr8.cpp
+++ pr8.cpp
@@ -1,33 +1,49 @@
-// ConsoleApplication20.cpp: определяет точку входа для консольного приложения.
-//
 
 
 #include "stdafx.h"
 #include "iostream"
 #include "cstring"
+
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
-{int t,n=10;
-char s[]="545";
-char s1[]="545";
-cout<<"Input  string";
-cin.getline(s,n);
-	int l=strlen(s);
-	int* m=new int[l];
-	int *m1=new int[l];
-	for(int i=0; i<l;i++)
-	{m[i]=0;m1[i]=0;}
-	t=0;
-	for(int i=0; i<l;i++)
+{
+	int count = 0, maxSize = 100;
+char str[100];
+
+cout<<"Input  string"<<endl;
+cin.getline(str, maxSize);
+cout<<endl; 
+	int length = strlen(str);
+	int* m = new int[length];
+	int *m1 = new int[length];
+	for(int i = 0; i < length;i++)
+	{
+		m[i] = 0;m1[i] = 0;
+	}
+	char* str1 = new char [length];
+	for(int i = 0; i < length;i++)
 		
-		if(m[i]==0) 
-		{m1[t]=1;m[i]=1;s1[t]=s[i];
+		if(m[i] == 0) 
+		{
+			m1[count] = 1;m[i] = 1;str1[count] = str[i];
 			
-			for(int k=i+1;k<l;k++)
-				if(s[k]==s[i])
-				{m[k]=1;m1[t]++;}
-		t++;}
+			for(int k = i + 1;k < length;k++)
+				if(str[k] == str[i])
+				{
+					m[k] = 1;m1[count]++;
+				}
+		count++;
+		}
 	
-		for(int i=0; i<t;i++)
-			cout<<s1[i]<<"    "<<m1[i]<<endl;
+		for(int i = 0; i < count;i++)
+			cout<<str1[i]<<"    "<<m1[i]<<endl;
+		cout<<endl; 
+		cin>>count;
+		delete[] m;
+		m=0;
+		delete[] m1;
+		m1=0;
+		delete[] str1;
+		str1=0;
+		return 0;
 }
