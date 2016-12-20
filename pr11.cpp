48ec6f841e4137211b400625c8b815e738236ae3
Zmenkov Dima <dimamatem@gmail.com>
2016-12-20 13:14:59 +0200

    remade pr11.cpp

--- pr11.cpp
+++ pr11.cpp
@@ -1,38 +1,53 @@
-// ConsoleApplication23.cpp: определяет точку входа для консольного приложения.
-//
 
-#include "stdafx.h"
+#include "stdafx.h" 
 #include "iostream"
 #include "cstring"
+
 using namespace std;
 
-bool test(char c)
-{for(char i='a';i<='z';i++)
-		if (i==c) return true;
-	for(char i='A';i<='Z';i++)
-if (i==c) return true;
-return false;}
+bool test(char ch)
+
+{
+	return(ch >= 'a')&&(ch <= 'z')||(ch >= 'A')&&(ch <= 'Z');
+}
+
 int _tmain(int argc, _TCHAR* argv[])
-{int pos,n=30;
-char *s=new char[n];
+{
+	int pos=0, maxSize = 30, strCount = 0;
+char *str = new char[maxSize];
 cout<<"Input  string"<<endl;
-cin.getline(s,n);
-	int l=strlen(s);
-	for(int i=0;i<l;i++)
-	if (s[i]=='.') pos=i;
-		
-	if(test(s[0])) 
-	{for(int i=0;i<pos;i++)
-	s[i]=s[i+1];pos--;
-	}
-	for(int i=0;i<pos;i++)
-	if((!test(s[i]))&&(test(s[i+1]))) 
-		{for(int j=i+1;j<pos;j++)
-		s[j]=s[j+1];pos--;
-			cout<<endl;
-	    }
+cin.getline(str, maxSize);
+	int  length = strlen(str);
+   for(int i = 0;i < length;i++)
+	if (str[i] == '.')  length = i;
+	
+	pos = length;
+	int t=0;
+	bool flag = 0;
+		if(test(str[0])) 
+	{
+		flag=true;pos--;
+		}
+		for(int i = 0;i < length ;i++)
+		{if(flag)
+		     {
+			    flag = false;continue;
+		     }
+		if((i > 0)&&(!test(str[i-1])) && (test(str[i]))) 
+		    {
+			  continue;
+		    }
+		 
+			str[strCount] = str[i];
+		strCount++;
+		}
 
 cout<<"new string"<<endl;
-for(int i=0;i<=pos;i++)
-cout<<s[i];
+for(int i = 0;i < strCount;i++)
+cout<<str[i];
+cout<<endl;
+delete[] str;
+str = 0;
 }
+
+
