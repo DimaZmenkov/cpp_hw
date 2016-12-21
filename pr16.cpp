6f364a22a6f72f276b688f1b099d28502903f5d6
Zmenkov Dima <dimamatem@gmail.com>
2016-12-21 12:23:08 +0200

    remade pr16.cpp

--- pr16.cpp
+++ pr16.cpp
@@ -5,26 +5,31 @@
 #include "iostream"
 using namespace std;
 double det(double**a)
-{double d=a[0][0]*a[1][1]*a[2][2]-a[2][0]*a[1][1]*a[0][2]-a[0][0]*a[2][1]*a[1][2];
-d+=-a[2][2]*a[1][0]*a[0][1]+a[0][2]*a[1][0]*a[2][1]+a[2][0]*a[0][1]*a[1][2];
-return d;
+
+{
+	double determinant = a[0][0] * a[1][1] * a[2][2] - a[2][0] * a[1][1] * a[0][2] - a[0][0] * a[2][1] * a[1][2];
+determinant += -a[2][2] * a[1][0] * a[0][1] + a[0][2] * a[1][0] * a[2][1] + a[2][0] * a[0][1] * a[1][2];
+return determinant;
 }
 
 	int _tmain(int argc, _TCHAR* argv[])
-{int n=3;
-
-
-
-double**a=new double*[n];
-for(int i=0;i<n;i++)
-	a[i]=new double[n];
+   {
+	int size = 3;
+double**aArray = new double*[size];
+for(int i = 0;i < size;i++)
+	aArray[i] = new double[size];
 cout<<"Input matrix "<<endl;
 
 	
-for(int i=0;i<n;i++)
-for(int j=0;j<n;j++)
-	cin>>a[i][j];
+for(int i = 0;i < size;i++)
+for(int j = 0;j < size;j++)
+	cin>>aArray[i][j];
 cout<<endl;
-cout<<det(a)<<endl;
+cout<<det(aArray)<<endl;
+for (int i = 0; i < size; i++) 
+{
+  delete [] aArray[i];
+}
+delete [] aArray;
 return 0;
 }
