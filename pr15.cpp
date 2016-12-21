c9cbc7436d3fd73745c71d109b6f0d26d4af96ca
Zmenkov Dima <dimamatem@gmail.com>
2016-12-21 12:10:17 +0200

    remade pr15.cpp

--- pr15.cpp
+++ pr15.cpp
@@ -7,28 +7,52 @@
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
-{int n,m;
-cout<<"Input n i m"<<endl;
+
+{
+	int n,m;
+
+	cout<<"Input n i m"<<endl;
 cin>>n>>m;
-int**a=new int*[n];
-for(int i=0;i<n;i++)
-	a[i]=new int[m];
-int**a1=new int*[m];
-for(int i=0;i<m;i++)
-	a1[i]=new int[n];
-	cout<<"Input array"<<endl;
-for(int i=0;i<n;i++)
-for(int j=0;j<m;j++)
-	cin>>a[i][j];
+
+int**aArray = new int*[n];
+
+for(int i = 0;i < n;i++)
+	aArray[i] = new int[m];
+
+int**a1Array = new int*[m];
+for(int i = 0;i < m;i++)
+	a1Array[i] = new int[n];
+	
+cout<<"Input array"<<endl;
+for(int i = 0;i < n;i++)
+for(int j = 0;j < m;j++)
+	cin>>aArray[i][j];
+
 cout<<endl;
-for(int i=0;i<n;i++)
-for(int j=0;j<m;j++)
-	a1[j][n-i-1]=a[i][j];
-for(int i=0;i<m;i++)
-{for(int j=0;j<n;j++)
-	cout<<a1[i][j]<<" ";
+for(int i = 0;i < n;i++)
+for(int j = 0;j < m;j++)
+	a1Array[j][n - i - 1] = aArray[i][j];
+
+for(int i = 0;i < m;i++)
+{
+	for(int j = 0;j < n;j++)
+	cout<<a1Array[i][j]<<" ";
 cout<<endl;
 }
+
+for (int i = 0; i < n; i++) 
+{
+  delete [] aArray[i];
+}
+delete [] aArray;
+
+for (int i = 0; i < m; i++)
+{
+  delete [] a1Array[i];
+}
+delete [] a1Array;
+
 return 0;
+
 }
 
