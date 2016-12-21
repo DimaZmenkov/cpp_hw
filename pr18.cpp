ce04678f6271bd0e1759109d712c2bcb6690970a
Zmenkov Dima <dimamatem@gmail.com>
2016-12-21 13:35:17 +0200

    remade pr18.cpp

--- pr18.cpp
+++ pr18.cpp
@@ -1,55 +1,65 @@
+
+
 // ConsoleApplication34.cpp: определяет точку входа для консольного приложения.
 //
 
+
+
 #include "stdafx.h"
 #include "iostream"
+
 using namespace std;
 
 	struct Professor
-	{char name[20];
+	{
+		char name[20];
             unsigned short age;
             unsigned int salary;
 	};
+	
 	struct student
-{char name[20];
+{
+	char name[20];
             unsigned short age;
             float gpa;
 Professor* supervisor;
 };
 int _tmain(int argc, _TCHAR* argv[])
-{int n,m,j;
-float s;
+
+{
+	int sizeGroup, quantityProffessors, count;
+float sum;
 cout<<"Input quantity of students"<<endl;
-cin>>n;
-student* group= new student[n];
-for(int i=0;i<n;i++)
+cin>>sizeGroup;
+student* group = new student[sizeGroup];
+for(int i = 0;i < sizeGroup;i++)
 	{cout<<" Input name"<<endl;
 	cin>>group[i].name;
 cout<<" Input age"<<endl;
 cin>>group[i].age;
 cout<<" Input gpa"<<endl;
 cin>>group[i].gpa;
-}	
-s=0;
-for(int i=0;i<n;i++)
-s+=group[i].gpa;
-s/=n;
+    }	
+sum=0;
+for(int i = 0;i < sizeGroup;i++)
+sum+=group[i].gpa;
+sum /= sizeGroup;
 cout<<"the average group score"<<endl;
-cout<<s<<endl;
-student stmax,stmin=group[0];
-stmax=stmin;
-for(int i=1;i<n;i++)
-	{if(stmax.gpa<group[i].gpa) stmax=group[i];
-	if(stmin.gpa>group[i].gpa) stmin=group[i];
+cout<<sum<<endl;
+student stMax,stMin=group[0];
+stMax=stMin;
+for(int i = 1;i < sizeGroup;i++)
+	{if(stMax.gpa < group[i].gpa) stMax = group[i];
+	if(stMin.gpa > group[i].gpa) stMin = group[i];
     }
 cout<<"Student with max average group score"<<endl;
-cout<<stmax.name<<endl;
+cout<<stMax.name<<endl;
 cout<<"Student with min average group score"<<endl;
-cout<<stmin.name<<endl;
+cout<<stMin.name<<endl;
 cout<<"Input quantity of proffesors"<<endl;
-cin>>m;
-Professor *pr=new Professor[m];
-for(int i=0;i<m;i++)
+cin>>quantityProffessors;
+Professor *pr = new Professor[quantityProffessors];
+for(int i = 0;i < quantityProffessors;i++)
 	{cout<<" Input name"<<endl;
 	cin>>pr[i].name;
 cout<<" Input age"<<endl;
@@ -57,12 +67,21 @@ cin>>pr[i].age;
 cout<<" Input salary"<<endl;
 cin>>pr[i].salary;
 }	
-for(int i=0;i<n;i++)
-{j=i%m;
-group[i].supervisor=pr+j;
-if(group[i].supervisor->age>50) cout<<group[i].name<<endl;
+for(int i = 0;i < sizeGroup;i++)
+{
+	count = i % quantityProffessors;
+group[i].supervisor = pr + count;
+if(group[i].supervisor -> age > 50) cout<<group[i].name<<endl;
 }
-cin>>j;
-return 0;
+cin>>count;
+
+
+
+for(int i = 0;i < sizeGroup;i++)
+	delete group[i].supervisor;
+delete[] group;
+delete[] pr;
+	return 0;
 }
+
 
