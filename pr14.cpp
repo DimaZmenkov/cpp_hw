e8ca7a5b4b99b730f7c2f1f2ec6808dba7da9cf6
Zmenkov Dima <dimamatem@gmail.com>
2016-12-25 19:56:14 +0200

    remade pr14.cpp

--- pr14.cpp
+++ pr14.cpp
@@ -1,93 +1,146 @@
-// ConsoleApplication32.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include "iostream"
 #include "fstream"
 #include "cstring"
 using namespace std;
-bool test1(char c)
-{for(char i='a';i<='z';i++)
-		if (i==c) return true;
-	return false;}
-bool test2(char c)
-{for(char i='A';i<='Z';i++)
-		if (i==c) return true;
-	return false;}
-bool test3(char c)
-{if (c==0) return true;
-	for(char i='1';i<='9';i++)
-		if (i==c) return true;
-	return false;}
+bool test1(char ch)
+
+{
+	return(ch >= 'a') && (ch <= 'z');
+}
+
+bool test2(char ch)
+
+{
+	return(ch >= 'A') && (ch <= 'Z');
+}
+
+bool test3(char ch)
+
+{
+	return(ch >= '1') && (ch <= '9')|| (ch == '0');
+}
+
 int _tmain(int argc, _TCHAR* argv[])
-{const int n=8;
-	int i,k,k1,ff,f1,k2,f2,f3,ff1=0;
-	bool fs=false;
-char* pass=new char[n];
+{const int sizePass = 8;
+	const int sizeTable = 'z'-'a'+'Z'-'A'+3*('9'-'1') + 9;
+int count0, count, count1,  flag1, count2, flag2, flag3;
+	bool fs = false;
+	bool flag = false, flag01;
+	char pass[sizePass] ;
 char ch;
-	char* t=new char[83];
+	char table[sizeTable] ;
 	ofstream fout;
 	ifstream fin;
 	fin.open("D:/file.txt");
-	k=0;
-	for(char i='a';i<='z';i++)
-	{t[k]=i; k++;}
-	for(char i='A';i<='Z';i++)
-	{t[k]=i; k++;}
-	for(char i='1';i<='9';i++)
-	for(int j=0;j<3;j++)
-		{t[k]=i; k++;}
-		for(int j=0;j<3;j++)
-		{t[k]='0'; k++;}
+	count = 0;
+	for(char i = 'a';i <= 'z';i++)
+	{
+		table[count] = i; count++;
+	}
+	for(char i = 'A';i <= 'Z';i++)
+	{
+		table[count] = i; count++;
+	}
+	for(char i = '1';i <= '9';i++)
+	for(int j = 0;j < 3;j++)
+		{
+			table[count] = i;count++;
+	    }
+		for(int j = 0;j < 3;j++)
+		   {
+			table[count] = '0'; count++;
+		   }
 			
-		t[k]='_'; 
-			k=0;
+		table[count] = '_'; 
+			count = 0;
 		
 			while(fin>>ch)
-		if(ch!='/n') k++;
+		if(ch != '/n') count++;
 		     
 			fin.close();
 			fin.open("D:/file.txt");
-			 char *buff=new char[k+8];
-	for(int i=0;i<k;i++)
-		buff[i]='0';    
-		k=0;
+			 char *buff = new char[count + 8];
+	for(int i = 0;i < count;i++)
+		buff[i] = '0';    
+		count = 0;
 			while(fin>>ch)
-		{if(ch!='/n') {
-		buff[k]=ch;k++;
-		              }           
-		} 
-		 ff=0;
-		while(ff==0 )
+		
+			if(ch != '/n') 
+			{
+		buff[count] = ch;count++;
+		    }           
+		
+		
+		while( !flag )
 		{
-			f1=0;f2=0;f3=0;ff1=0;
-			for(int j=0;j<n;j++)
-			{	k2=rand()%83; pass[j]=t[k2];
+			flag1 = 0;flag2 = 0;flag3 = 0;flag01 = false;
+			for(int j = 0;j < sizePass;j++)
+			
+			{	
+			count2 = rand() % sizeTable; pass[j] = table[count2];
 				
-			if(ff1==0){if(j>2) if(f1+f2+f3==3) ff1=1;
-				if(f1==0) if(test1(pass[j])) {f1=1;continue;}
-		if(f2==0) if(test2(pass[j])) {f2=1;continue;}
-		if(f3==0) if(test3(pass[j])) {f3=1;continue;}
-			          }
-		    }
-		if(ff1==0) continue;
-			k1=0;
-		ff=1;i=0;
-		while(i<k)
+			if(!flag01)
+			         
+			          {
+				if(j > 2) if(flag1 + flag2 + flag3 == 3) flag01 = true;
+				if(!flag01) if(test1(pass[j])) 
+				
+				{
+					flag1 = 1;continue;
+				}
 
-		{if(buff[i]!=pass[k1]){i+=n-k1;k1=0;}
-			else {k1++;i++;}
-		if (k1==8) {ff=0;break;}
+		if(flag2 == 0) if(test2(pass[j])) 
+		
+		{
+			flag2 = 1;continue;
+		}
+
+		if(flag3 == 0) if(test3(pass[j])) 
+		
+		{
+			flag3 = 1;continue;
+		}
+
+			          }
+
+		    }
+		if(!flag01) continue;
+			count1 = 0;
+		flag = true;count0 = 0;
+		while(count0 < count)
+
+		{
+			if(buff[count0] != pass[count1])
+			      
+			      {
+				count0 += sizePass - count1;count1 = 0;
+			      }
+
+			else 
+			
+			{
+				count1++;count0++;
+			}
+
+		if (count1 == 8)
+		  
+		  {
+			flag = false;break;
+		  }
+
 		}
 			
 		}
 	fin.close();
 		cout<<"pass"<<endl;
-  
+
 		fout.open("D:/file.txt");
-	for(int j=0;j<n;j++)
-  {buff[k+j]=pass[j];cout<<pass[j]<<" ";}
- for(int j=0;j<k+n;j++)
+	for(int j = 0;j < sizePass;j++)
+  { 
+	  buff[count + j] = pass[j];cout<<pass[j]<<" ";
+  }
+ for(int j = 0;j < count +  sizePass ;j++)
 	fout<<buff[j];
 	fout.close();
 return 0;
