f84fbe66edb85026340066d823032b05fa8069c1
Zmenkov Dima <dimamatem@gmail.com>
2016-12-25 17:45:08 +0200

    last remade pr12.cpp

--- pr12.cpp
+++ pr12.cpp
@@ -8,20 +8,21 @@ using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
-{int temp, length, size = 12;
+{
+	int temp = 0, size = 12;
 cout<<"Input size of array"<<endl;
 	cin>>size;
-int *mArray = new int[size];
-
+int *mArray = new int[size]();
 cout<<"Input  array"<<endl;
-for(int i=0;i < size;i++)
+for(int i = 0;i < size;i++)
 	cin>>mArray[i];
 	
-for(int i=0;i < size-1;i++)
-			if(!(i&1)) {temp = mArray[i];mArray[i] = mArray[i+1];mArray[i+1] = temp;}
+for(int i = 0;i < size-1;i++)
+			if(!(i&1)) {temp = mArray[i];mArray[i] = mArray[i + 1];mArray[i + 1] = temp;}
 cout<<"new  array"<<endl;
 for(int i = 0;i < size;i++)
-cout<<mArray[i];
-	return 0;	
+cout<<mArray[i]<<" ";
+	cout<<endl;
+return 0;	
 	    
 }
