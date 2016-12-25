15bfbabf9e05fc3a79fa55f2b95ddea412dd8a7a
Zmenkov Dima <dimamatem@gmail.com>
2016-12-25 17:26:16 +0200

    remade pr9.cpp

--- pr9.cpp
+++ pr9.cpp
@@ -13,25 +13,16 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 
 {
-	int count, numberEncryption, size = 10;
-const int tableSize = 36;
-
-char str1 [tableSize];
-cout<<"Input  size of string"<<endl;
-cin>>size;
-char *str = new char[size];
+	int  numberEncryption;
+	const int size = 50;
+	
+	const int tableSize = 36;
+	char str[size] ;
+const char str1 [] = "abcdefghijklmnopqrstuvwxyz0123456789";
 cout<<"Input  string"<<endl;
 cin.getline(str,size);
-	int length = strlen(str);count=0;
-	for(char i = 'a';i <= 'z';i++)
-	{
-		str1[count] = i;count++;}
-	for(char i = '0';i <= '9';i++)
-	{
-		str1[count] = i;count++;}
-	cout<<"table"<<endl;
-	for(int j = 0;j < tableSize;j++)
-		cout<<str1[j];
+	int length = strlen(str);
+	
 	for(int i = 0;i < length;i++)
     for(int j = 0;j < tableSize;j++)
 if(str[i] == str1[j])
@@ -40,7 +31,6 @@ if(str[i] == str1[j])
 }
 cout<<endl;
 cout<<"new string"<<endl;
-cout<<str;
-delete[] str;
+cout<<str<<endl;
 return 0;
 }
