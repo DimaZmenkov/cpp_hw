0a2577d951607dd912a94299dc1f52ce7c340d6e
Zmenkov Dima <dimamatem@gmail.com>
2016-12-25 14:33:29 +0200

    remade pr8.cpp

--- pr8.cpp
+++ pr8.cpp
@@ -14,36 +14,36 @@ cout<<"Input  string"<<endl;
 cin.getline(str, maxSize);
 cout<<endl; 
 	int length = strlen(str);
-	int* m = new int[length];
-	int *m1 = new int[length];
+	int* mArray = new int[length];
+	int *m1Array = new int[length];
 	for(int i = 0; i < length;i++)
 	{
-		m[i] = 0;m1[i] = 0;
+		mArray[i] = 0;m1Array[i] = 0;
 	}
 	char* str1 = new char [length];
 	for(int i = 0; i < length;i++)
 		
-		if(m[i] == 0) 
+		if(mArray[i] == 0) 
 		{
-			m1[count] = 1;m[i] = 1;str1[count] = str[i];
+			m1Array[count] = 1;mArray[i] = 1;str1[count] = str[i];
 			
 			for(int k = i + 1;k < length;k++)
 				if(str[k] == str[i])
 				{
-					m[k] = 1;m1[count]++;
+					mArray[k] = 1;m1Array[count]++;
 				}
 		count++;
 		}
 	
 		for(int i = 0; i < count;i++)
-			cout<<str1[i]<<"    "<<m1[i]<<endl;
+			cout<<str1[i]<<"    "<<m1Array[i]<<endl;
 		cout<<endl; 
 		cin>>count;
-		delete[] m;
-		m=0;
-		delete[] m1;
-		m1=0;
+		delete[] mArray;
+		//mArray=0;
+		delete[] m1Array;
+		//m1Array=0;
 		delete[] str1;
-		str1=0;
+		//str1=0;
 		return 0;
 }
