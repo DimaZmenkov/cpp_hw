8f0cd6d74497b6b114eb424dde8c48138bdfbb3f
Zmenkov Dima <dimamatem@gmail.com>
2016-12-27 18:58:16 +0200

    Initial commit.

---
+++ test12.cpp
@@ -0,0 +1,57 @@
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class HTMLElement
+{
+public:
+             unsigned Width() const;
+			 unsigned Height() const;
+			 bool Hidden() const;
+			 virtual string Render() = 0;
+};
+class HTMLButtonElement: public  HTMLElement
+{ 
+	public:
+	const string& Title() const;
+	string Render() override;
+
+};
+class HTMLImageElement:public  HTMLElement
+{
+	public:
+	const string& Url() const;
+	string Render() override
+	{
+		string str = "<img src=../../smiley.gif height=42 width=4> ";
+	return str;
+	}
+};
+class HTMLTextAreaElement:public  HTMLElement
+{
+	public:
+	const string& Content() const;
+	string Render() override
+	{
+	string str = "<textarea>content-of-the-text-area</textarea>";
+return str;
+	}
+};
+string HTMLButtonElement::Render()
+{string str = "<button>title-of-the-button</button> ";
+	return str;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+
+	{
+		HTMLElement* pButton = new HTMLButtonElement();
+	HTMLElement* pImage  = new HTMLImageElement();
+    HTMLElement* pTextArea = new HTMLTextAreaElement();
+	pButton -> Render();
+	 pImage -> Render();
+	 pTextArea -> Render();
+return 0;
+}
