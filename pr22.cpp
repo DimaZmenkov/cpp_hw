4919bbcc5c0c7ffe2ff457558f5a1def1de40fe9
Zmenkov Dima <dimamatem@gmail.com>
2016-12-29 23:54:31 +0200

    remade pr22.cpp

--- pr22.cpp
+++ pr22.cpp
@@ -1,3 +1,6 @@
+// ConsoleApplication82.cpp: определяет точку входа для консольного приложения.
+//
+
 // ConsoleApplication49.cpp: определяет точку входа для консольного приложения.
 //
 
@@ -7,59 +10,89 @@
 
 #include "stdafx.h"
 #include "iostream"
+
 #include "cstring"
+
 using namespace std;
-class String { 
+
+class String 
+{ 
+	
 public:
 	String ( char*  str);
-	~String();
-	inline int GetLength() const;
-	String& operator=(const String& rhs);
-	char* GetString() const; 
+	~String()
+	{
+         for (int i = 0;i < count; i++)	
+			 if (m_string == staticCharArray[i])
+			 {
+                     if(--staticCountArray[i]==0) delete[] staticCharArray[i]; 
+			 }
+	}
+	//inline int GetLength() const;
+	String& operator = (const String& rhs);
+	char* getString() const; 
+
+
 private:
-char*itsString;
-int itsLength;
+	static char* staticCharArray[100];
+static int count;
+static bool flag;
+static int staticCountArray[100];
+char*m_string;
+//int m_length;
 };
 String::String ( char *  str)
-	{itsString=str;
-	itsLength=strlen(str);
-	}
-String::~String()
-{cout<<"String destructor"<<endl;}
-String& String::operator=(const String& rhs)
-{if(this==&rhs) return *this;
-itsString=rhs.itsString;
-itsLength=rhs.itsLength;
-return *this;
-};
-char*String::GetString() const
-{return itsString;}
-	int String::GetLength() const
- {return itsLength;}
-int _tmain(int argc, _TCHAR* argv[])
-{int n=40;
-	char s[]="545";
-	char s1[]="545";
-	cout<<"Input first string"<<endl;
-	cin.getline(s,n);
-String a(s);
-cout<<"Input second string"<<endl;
-	cin.getline(s1,n);
-String b(s1);
-b=a;
-cout<<"a="<<endl;
-char*sa=a.GetString();
-char*sb=b.GetString();
-int la=a.GetLength();
-int lb=b.GetLength();
-for(int i=0;i<la;i++)
-	cout<<sa[i];
-cout<<endl;
-cout<<"b="<<endl;
-	for(int i=0;i<lb;i++)
-	cout<<sb[i];
-	cout<<endl;
-cout<<"&a="<<&a<<endl;
-	cout<<"&b="<<&b<<endl;
-return 0;
+
+	
+//m_length(strlen(str))
+{
+	bool flag = false;
+	for (int i = 0;i < count; i++)	
+	  if (strcmp (str, staticCharArray[i] )==0)
+		  {
+			  str = staticCharArray[i];staticCountArray[i]++; flag = true;break;
+	      }
+			  
+	      
+	 if (!flag)
+	  {
+		  staticCharArray[count] = str; staticCountArray[count++]++;
+	  }	   
+	
 }
+
+String& String::operator = (const String& rhs)
+{
+	if(this == &rhs) 
+		return *this;
+	for (int i = 0;i < count; i++)	
+	 // if (strcmp (rhs.m_string, staticCharArray[i] )==0)
+		  if (rhs.m_string == staticCharArray[i]) 
+		  {
+			  m_string = staticCharArray[i];staticCountArray[i]++; return *this;
+	      }
+//return *this;
+
+}
+
+char*String::getString() const
+{
+	return m_string;
+}
+	/*int String::getLength() const
+ {
+	 return m_length;
+}*/
+char* String::  staticCharArray[100];
+int String:: count;
+bool String:: flag;
+int String:: staticCountArray[100];
+int _tmain(int argc, _TCHAR* argv[])
+{
+ String str1("123");
+	String str2 = str1;
+	
+
+	return 0;
+}
+
