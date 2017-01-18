72d13cac04a5eea4b1a31711c9f0002d9a633a4e
Zmenkov Dima <dimamatem@gmail.com>
2017-01-18 21:50:02 +0200

    last remade pr27.cpp

--- pr27.cpp
+++ pr27.cpp
@@ -12,7 +12,10 @@ int _tmain(int argc, _TCHAR* argv[])
  string str[7] = { "I", "or", "and", "that", "the", "of", "not"};
  file.open("D:/test.txt");
 	string word;
+	size_t pos;
+	int i;
 	map<string, int> words;
+	map<int, string> words1;
 	while(file>>word)
 	{
 		bool flag = true; 
@@ -23,15 +26,28 @@ int _tmain(int argc, _TCHAR* argv[])
 			break;
 		}
 		if(!flag) continue;	
+		pos = word.length()-1;
+		if(!((word[pos] >= 'A')&&(word[pos]<= 'Z')||(word[pos]>= 'a')&&(word[pos]<= 'z')))  
+			word.erase(pos,1);
 		
-		
-	++words[word];
-	}
-	for(auto it = words.rbegin();it != words.rend(); ++it)
-	cout<<(*it).first<<" "<<--(*it).second<<endl;
+	++words[word]; 
 	
-
- return 0;
+	}
+	int count = 0;
+	for(auto it = words.begin();it != words.end(); ++it)
+	{
+		words1[(*it).second] = (*it).first;
+		}
+ 
+	for(auto it = words1.rbegin();it != words1.rend(); ++it)
+	{
+		
+		cout<<(*it).second<<" "<<(*it).first<<endl;
+		
+	count++;
+	if (count >= 10) break;
+	}
+	return 0;
 }
 
 
