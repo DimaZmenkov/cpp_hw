831741309f0c1637cea870084c5f0a6806c56203
Zmenkov Dima <dimamatem@gmail.com>
2017-01-24 22:48:47 +0200

    remade pr27.cpp

--- pr27.cpp
+++ pr27.cpp
@@ -23,21 +23,29 @@ int _tmain(int argc, _TCHAR* argv[])
 {
  ifstream file;
  string str[7] = { "I", "or", "and", "that", "the", "of", "not"};
+ 
  set<string> testSet( str, str + 6);
  file.open("D:/test.txt");
 	string word;
+	const string test1 ="0123456789+-_";
 	size_t pos;
 	map<string, int> words;
 	multimap<int, string> words1;
 	while(file>>word)
     
 	{
-			 transform (word.begin(), word.end(), word.begin(), ::tolower);
-		pos = word.length() - 1;
-		if(!((word[pos] >= 'a')&&(word[pos] <= 'z')))  
+		transform (word.begin(), word.end(), word.begin(), ::tolower );
+	int	pos = word.length() ;
+		//for(int j = 0;j < pos;j++)
+		if(!((word[pos] >= 'a')&&(word[pos] <= 'z')||(word[pos]=='+')))
+		{word.erase(pos, 1);pos--;}
+				
+		   if(pos > 1)
+				if(!((word[pos] >= 'a')&&(word[pos] <= 'z')||(word[pos] == '+')))
 			word.erase(pos, 1);
-				//pos--;
-		    if(binary_search(testSet.begin (), testSet.end(), word )) continue;
+				if(!((word[0] >= 'a')&&(word[0] <= 'z')||(word[0] == '+')))
+			word.erase(0, 1);
+		   if(binary_search(testSet.begin (), testSet.end(), word )) continue;
 			++words[word]; 
 	}
 
